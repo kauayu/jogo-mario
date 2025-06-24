5596679f67080521b1a8ff875694b5d7d76c8b04
kauayu <kaua.palmiro.ferreira@escola.pr.gov.br>
2025-06-24 16:31:08 -0300

    .

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
-const mario = document.querySelector(".mario")
-const pipe = document.querySelector(".pipe")
+const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
 
 const jump = () => {
     mario.classList.add("jump");
 
     setTimeout(() => {
         mario.classList.remove("jump");
-    }
-    ,500);
-}
+    }, 500);
+};
 
 const loop = setInterval(() => {
-const pipePosition = pipe.offsetleft;
-const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
 
-    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-        pipe.Style.animation = "none";
-        pipe.style.left = `${pipePosition}px`;
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        pipe.style.animation = "none";
+        pipe.style.left = ${pipePosition}px;
 
-        mario.Style.animation = "none";
-        mario.Style.left = `${pipePosition}px`;
+        mario.style.animation = "none";
+        mario.style.left = ${pipePosition}px;
 
-        mario.src= "./img/game-over.png";
+        mario.src = "./img/game-over.png";
         mario.style.width = "75px";
-        mario.style.marginleft = "50px";
+        mario.style.marginLeft = "50px";
 
         clearInterval(loop);
     }
-},10)
+}, 10);
+
 document.addEventListener("keydown", jump);
